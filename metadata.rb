3ebc2298c64ddacadce5566742734462f0ff4263
Kendrick Martin <Kendrick.Martin@Webtrends.com>
2013-06-13 10:54:05 -0700

    Initial commit to new zookeeper cookbook repo

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "Webtrends Inc."
+maintainer_email "devops@webtrends.com"
+license          "Apache 2.0"
+description      "Installs/Configures ZooKeeper"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.2.8"
+depends          "java", ">= 1.5"
+depends          "runit"
+
